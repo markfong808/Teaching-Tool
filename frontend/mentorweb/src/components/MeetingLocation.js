5e5b77132ed78f4bbf0a0f8dec4acf6d492d4a52
MuhammadCheema <cheema.muhammad17@gmail.com>
2024-02-12 23:16:33 -0800

    Finished MeetingLocation.js.

--- frontend/mentorweb/src/components/MeetingLocation.js
+++ frontend/mentorweb/src/components/MeetingLocation.js
@@ -6,12 +6,14 @@ import { Tooltip } from "./Tooltip.js";
 import Comment from "./Comment.js";
 import TimeRangePicker from "@wojtekmaj/react-timerange-picker";
 
-export default function MeetingLocation(param) {
+export default function MeetingLocation({isClassLocation, param}) {
     const [boxShown, setBoxShown] = useState(false);
 
     const [formData, setFormData] = useState({
         class_location: '',
-        class_recordings_link: ''
+        office_hours_location: '',
+        class_recordings_link: '',
+        office_hours_link: ''
     });
 
     // Meeting Location show box
@@ -25,7 +27,8 @@ export default function MeetingLocation(param) {
 
     const handleInputChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
-        param.functionPassed({
+        
+        param.functionpassed({
             name: e.target.name,
             value: e.target.value
         });
@@ -35,13 +38,13 @@ export default function MeetingLocation(param) {
         <div>
             <div className="flex flex-col w-1/2 p-5 m-auto border border-light-gray rounded-md shadow-md mt-5">
                 <div className="flex items-center">
-                    <label className="font-bold text-2xl">Set Class Location:</label>
+                    <label className="font-bold text-2xl">{isClassLocation ? "Set Class Location:" : "Set Office Location:"}</label>
                     <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-blue-600 ml-5" onClick={showBox}></input>
                     <span className="px-2 py-2 text-sm font-normal">In-Person?</span>
-                    {boxShown && (
+                    {boxShown &&(
                         <input className='border border-light-gray ml-2 text-sm font-normal'
-                            name="class_location"
-                            value={formData.class_location}
+                            name = {isClassLocation ? "class_location" : "office_hours_location"}
+                            value= {isClassLocation ? formData.class_location : formData.office_hours_location}
                             onChange={handleInputChange}
                         />
                     )}
@@ -49,10 +52,10 @@ export default function MeetingLocation(param) {
             </div>
             <div className="flex flex-col w-1/2 p-5 m-auto border border-light-gray rounded-md shadow-md mt-5">
                 <div className="flex items-center">
-                    <label className="font-bold text-2xl">Class Recording Link:</label>
+                    <label className="font-bold text-2xl">{isClassLocation ? "Class Recordings Link:" : "Virtual Meeting Link:"}</label>
                     <input className='border border-light-gray ml-2 text-sm font-normal'
-                            name="class_recordings_link"
-                            value={formData.class_recordings_link}
+                            name={isClassLocation ? "class_recordings_link" : "office_hours_link"}
+                            value={isClassLocation ? formData.class_recordings_link : formData.office_hours_link}
                             onChange={handleInputChange}
                         />
                 </div>
