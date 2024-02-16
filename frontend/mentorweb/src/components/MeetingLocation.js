dacacf084d8fd59a368b858205d8ec71f22ebb03
MuhammadCheema <cheema.muhammad17@gmail.com>
2024-02-16 00:07:06 -0800

    Changed the view of the Office and Class Location in MeetingLocation.js

--- frontend/mentorweb/src/components/MeetingLocation.js
+++ frontend/mentorweb/src/components/MeetingLocation.js
@@ -7,17 +7,9 @@ export default function MeetingLocation({isClassLocation, param, data, loadPage,
         class_location: '',
         office_hours_location: '',
         class_recordings_link: '',
-        office_hours_link: ''
+        office_hours_link: '',
+        class_link: ''
     });
-
-    // Meeting Location show box
-    const showBox = () => {
-        if (boxShown) {
-            setBoxShown(false);
-        } else {
-            setBoxShown(true);
-        }
-    };
 
     const handleInputChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -59,29 +51,40 @@ export default function MeetingLocation({isClassLocation, param, data, loadPage,
     return (
         <div className="w-2/3 m-auto">
             <div className="flex flex-col p-5 border border-light-gray rounded-md shadow-md mt-5">
-                <div className="flex items-center">
-                    <label className="whitespace-nowrap font-bold text-2xl">{isClassLocation ? "Set Class Location:" : "Set Office Location:"}</label>
-                    <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-blue-600 ml-5 mt-2" checked={boxShown} onChange={showBox}></input>
-                    <span className="whitespace-nowrap px-2 py-2 text-sm font-normal mt-2">In-Person?</span>
-                    {boxShown && (
-                        <input className='border border-light-gray ml-2 text-sm font-normal w-20 mt-2'
-                            name = {isClassLocation ? "class_location" : "office_hours_location"}
-                            value= {isClassLocation ? formData.class_location : formData.office_hours_location}
+                <div className="flex">
+                    <label className="whitespace-nowrap font-bold text-2xl mb-2">{isClassLocation ? "Set Class Location:" : "Set Office Location:"}</label>
+                </div>
+                    <div className="flex flex-row ">
+                        <label className="whitespace-nowrap">Location:</label>
+                            <input className='border border-light-gray ml-2 text-sm font-normal w-40 mt-1'
+                                name = {isClassLocation ? "class_location" : "office_hours_location"}
+                                value= {isClassLocation ? formData.class_location : formData.office_hours_location}
+                                onChange={handleInputChange}
+                            />
+                    </div>
+                    <div>
+                        <label className="whitespace-nowrap">Virtual Meeting Link:</label>
+                            <input className='border border-light-gray ml-2 text-sm font-normal w-40 mt-2'
+                                name={isClassLocation ? "class_link" : "office_hours_link"}
+                                value={isClassLocation ? formData.class_link : formData.office_hours_link}
+                                onChange={handleInputChange}
+                            />
+                    </div>
+                {isClassLocation && (
+                    <div>
+                        <label className="whitespace-nowrap">Class Recordings Link:</label>
+                        <input
+                            className='border border-light-gray ml-2 text-sm font-normal w-40 mt-2'
+                            name="class_recordings_link"
+                            value={formData.class_recordings_link}
                             onChange={handleInputChange}
                         />
-                    )}
+                    </div>
+                )}
                 </div>
+                   
             </div>
-            <div className="flex flex-col p-5 border border-light-gray rounded-md shadow-md mt-5">
-                <div className="flex items-center">
-                    <label className="font-bold text-2xl">{isClassLocation ? "Class Recordings Link:" : "Virtual Meeting Link:"}</label>
-                    <input className='border border-light-gray ml-2 text-sm font-normal mt-2'
-                            name={isClassLocation ? "class_recordings_link" : "office_hours_link"}
-                            value={isClassLocation ? formData.class_recordings_link : formData.office_hours_link}
-                            onChange={handleInputChange}
-                        />
-                </div>
-            </div>
-        </div>
+    
+       
     );
 }
