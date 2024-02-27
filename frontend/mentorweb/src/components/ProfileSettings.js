ae555e12dc8327b0680ec37f6e7d34b0885a8800
MuhammadCheema <cheema.muhammad17@gmail.com>
2024-02-26 18:58:24 -0800

    Profile done.

--- frontend/mentorweb/src/components/ProfileSettings.js
+++ frontend/mentorweb/src/components/ProfileSettings.js
@@ -8,30 +8,28 @@ export default function ProfileSettings() {
     const csrfToken = getCookie('csrf_access_token');
     const { user, setUser } = useContext(UserContext);
     const [textBoxShown, setTextBoxShown] = useState(false);
-    const [formData, setFormData] = useState({
-        name: user?.name || '',
-        pronouns: user?.pronouns || 'Undefined',
-        title: user?.title || '',
-        discord_id: user?.discord_id || '',
-        about: user?.about || '',
-        linkedin_url: user?.linkedin_url || '',
-        meeting_url: user?.meeting_url || '',
-    });
+    const [allProfileData, setallProfileData] = useState({});
+    const [formData, setFormData] = useState({});
 
     const [pronounsType, setPronounsType] = useState('');
     const [pronounsUserInput, setPronounsUserInput] = useState('');
     const [changesMade, setChangesMade] = useState(false);
 
     useEffect(() => {
-        //console.log(formData.pronouns);
+        if (user) {
+            fetchProfileInformation();
+        }
+    }, [user]);
+
+    useEffect(() => {
         if (formData.pronouns !== "Undefined" && formData.pronouns !== "He/Him" && formData.pronouns !== "She/Her" && formData.pronouns !== "They/Them") {
-            setPronounsType("Other");
             setPronounsUserInput(formData.pronouns);
         } else { // could have bug
             setPronounsType(formData.pronouns);
             setPronounsUserInput('');
         }
     }, [formData.pronouns]);
+
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -47,33 +45,21 @@ export default function ProfileSettings() {
             return;
         }
 
-        if (name === "pronouns" && newValue !== "Other") {
-            setPronounsType(newValue);
+        if (name === "pronouns" && pronounsType === "Other" && (newValue === "" || !newValue)) {
+            return;
         }
 
-        console.log(newValue);
-        console.log(pronounsType);
-        console.log(pronounsUserInput);
-        
-
-        if (name === "pronouns" && pronounsType === "Other" && pronounsUserInput !== '') {
-            console.log("Hit");
-            setFormData({ ...formData, [name]: pronounsUserInput });
-        } else {
-            console.log("Miss");
-            setFormData({ ...formData, [name]: newValue });
-        }
-
+        setFormData({ ...formData, [name]: newValue });
 
         // Check if changes were made
         const formIsSameAsUser = (
-            (name === 'name' && value === user.name) ||
-            (name === 'about' && value === user.about) ||
-            (name === 'pronouns' && value === user.pronouns) ||
-            (name === 'title' && value === user.title) ||
-            (name === 'discord_id' && value === user.discord_id) ||
-            (name === 'linkedin_url' && value === user.linkedin_url) ||
-            (name === 'meeting_url' && value === user.meeting_url)
+            (name === 'first_name' && value === allProfileData.first_name) ||
+            (name === 'about' && value === allProfileData.about) ||
+            (name === 'pronouns' && value === allProfileData.pronouns) ||
+            (name === 'title' && value === allProfileData.title) ||
+            (name === 'discord_id' && value === allProfileData.discord_id) ||
+            (name === 'linkedin_url' && value === allProfileData.linkedin_url) ||
+            (name === 'meeting_url' && value === allProfileData.meeting_url)
         );
 
         // Set changesMade to true if form data does not match initial user data
@@ -88,19 +74,30 @@ export default function ProfileSettings() {
         }
     }, [pronounsType]);
 
+
+
     const handleCancelChanges = () => {
         // Reset form data to initial user data
-        setFormData({
-            name: user.name || '',
-            pronouns: user.pronouns || '',
-            title: user.title || '',
-            discord_id: user.discord_id || '',
-            about: user.about || '',
-            linkedin_url: user.linkedin_url || '',
-            meeting_url: user.meeting_url || '',
-        });
+        setFormData(allProfileData);
         setChangesMade(false); // Reset changes made
     };
+
+    const fetchProfileInformation = async () => {
+        try {
+            const response = await fetch(
+                `/profile/instructor/${encodeURIComponent(user.id)}`,
+                {
+                    credentials: "include",
+                }
+            );
+            const fetchedProfileInformation = await response.json();
+            setallProfileData(fetchedProfileInformation);
+            setFormData(fetchedProfileInformation);
+        } catch (error) {
+            console.error("Error fetching information:", error);
+        }
+
+    }
 
     const handleSaveChanges = async () => {
         try {
@@ -122,7 +119,7 @@ export default function ProfileSettings() {
             setChangesMade(false); // Reset changes made
             // Update the user context with the new data
             const updatedUser = await response.json();
-            setUser(updatedUser);
+            setallProfileData(updatedUser);
         } catch (error) {
             console.error('Error updating profile:', error);
         }
@@ -132,6 +129,7 @@ export default function ProfileSettings() {
     if (!user) {
         return <div>Loading user data...</div>;
     }
+
 
     return (
         <div className="flex flex-col w-2/3 m-auto">
@@ -147,8 +145,8 @@ export default function ProfileSettings() {
                     </div>
 
                     <input className='border border-light-gray mb-3'
-                        name="name"
-                        value={formData.name}
+                        name="first_name"
+                        value={formData.first_name}
                         onChange={handleInputChange}
                     />
 
@@ -176,7 +174,18 @@ export default function ProfileSettings() {
                         )}
                     </div>
 
-                    {user?.account_type === 'mentor' && (
+
+                    {allProfileData.account_type === 'student' && (
+                        <div className='flex flex-col mt-3'>
+                            <label className='font-bold'>Student ID</label>
+                            <label>{allProfileData.student_id ? allProfileData.student_id : "No known Student ID"}</label>
+                        </div>
+
+                    )}
+
+
+
+                    {allProfileData.account_type === 'mentor' && (
                         <div className='mt-3'>
                             <label className='font-bold inline-block'>Title</label>
                             <select
@@ -197,10 +206,10 @@ export default function ProfileSettings() {
                     )}
 
 
-                    <label className='font-bold mt-3'>Email</label>
+                    <label className='font-bold'>Email</label>
                     <input className='bg-gray border border-light-gray mb-3'
                         name='email'
-                        value={user?.email}
+                        value={formData.email}
                         disabled
                     />
 
@@ -214,26 +223,30 @@ export default function ProfileSettings() {
                     <label className='font-bold'>Account Type</label>
                     <input className='bg-gray border border-light-gray mb-3'
                         name='type'
-                        value={capitalizeFirstLetter(user?.account_type)}
+                        value={capitalizeFirstLetter(allProfileData.account_type)}
                         disabled
                     />
 
+                    {allProfileData.account_type === 'mentor' && (
+                        <div className='flex flex-col'>
+                            <div>
+                                <label className='font-bold'>Your Personal Meeting URL&nbsp;</label>
+                                <Tooltip text="Please provide only one URL of your choice - Zoom, Teams, etc.">
+                                    <span>
+                                        ⓘ
+                                    </span>
+                                </Tooltip>
+                            </div>
 
-                    <div>
-                        <label className='font-bold'>Your Personal Meeting URL&nbsp;</label>
-                        <Tooltip text="Please provide only one URL of your choice - Zoom, Teams, etc.">
-                            <span>
-                                ⓘ
-                            </span>
-                        </Tooltip>
-                    </div>
 
-                    <input className='border border-light-gray'
-                        type="text"
-                        name="meeting_url"
-                        value={formData.meeting_url}
-                        onChange={handleInputChange}
-                    />
+                            <input className='border border-light-gray'
+                                type="text"
+                                name="meeting_url"
+                                value={formData.meeting_url}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                    )}
 
                     {changesMade && (
                         <div className="flex flex-row justify-end my-5">
